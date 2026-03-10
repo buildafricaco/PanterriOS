63b812975d1d6583d93a1af0eb6148466f7d0b26
jaymhorsh <jaji.moshood1297@gmail.com>
2026-03-10 15:18:19 +0100

    feat: Implement investment details components wired with api endpoint

--- hook/investment-management/index.ts
+++ hook/investment-management/index.ts
@@ -1,2 +1,5 @@
 export * from './useCreateInvestment';
 export * from './useRetrieveInvestments';
+export * from './useRetrieveInvestmentDetails';
+export * from './useUpdateInvestment';
+export * from './useDeleteInvestment';
